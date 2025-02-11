f106b4401c8c41e13c3387d29f1b5aa00caaa55e
valchys <vcorrales1@eafit.edu.co>
2025-02-11 11:49:34 -0500

    Step B: about page

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
+Route::get('/about', function () {
+    $data1 = "About us - Online Store";
+    $data2 = "About us";
+    $description = "This is an about page ...";
+    $author = "Developed by: Valeria Corrales Hoyos";
+    return view('home.about')->with("title", $data1)
+      ->with("subtitle", $data2)
+      ->with("description", $description)
+      ->with("author", $author);
+})->name("home.about");
+
