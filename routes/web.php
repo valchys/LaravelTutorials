201e9f323c726d978eb2bc6f040024e13d55024b
valchys <vcorrales1@eafit.edu.co>
2025-02-17 02:13:40 -0500

    Section D: product creation

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ Route::get('/about', function () {
       ->with("author", $author);
 })->name("home.about");
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
+Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name("product.create");
+Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name("product.save");
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
 
