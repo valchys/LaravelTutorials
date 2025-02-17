7c8566582b9675924ec2eae6c79e98bfec6223e0
valchys <vcorrales1@eafit.edu.co>
2025-02-17 03:02:51 -0500

    Activity 4: Display produc prices

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,10 +8,10 @@ use Illuminate\View\View;
 class ProductController extends Controller
 {
     public static $products = [
-        ["id"=>"1", "name"=>"TV", "description"=>"Best TV"],
-        ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone"],
-        ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast"],
-        ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses"]
+        ["id"=>"1", "name"=>"TV", "description"=>"Best TV", "price"=>"$ 8.000.000"],
+        ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone", "price"=>"$ 7.000.000"],
+        ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast", "price"=>"$ 1.000.000"],
+        ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses", "price"=>"$ 1.500.000"]
     ];
 
     public function index(): View
