2e08a7fd09e1a429bcc1a180a00bf2d295a8fa8e
valchys <vcorrales1@eafit.edu.co>
2025-02-17 02:36:55 -0500

    Activity 2: Contact section

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ Route::get('/about', function () {
       ->with("description", $description)
       ->with("author", $author);
 })->name("home.about");
+Route::get('/contact', function () {
+  $data1 = "Contact us - Online Store";
+  $data2 = "Contact us";
+  $description = "You can contact us through ...";
+  $email = "exampleapp@email.com";
+  $address = "100 Flushcombe Road,Albury 2148";
+  $phone = "+123 1002009";
+  return view('home.contact')->with("title", $data1)
+    ->with("subtitle", $data2)
+    ->with("description", $description)
+    ->with("email", $email)
+    ->with("address", $address)
+    ->with("phone", $phone);
+})->name("home.contact");
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
 Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name("product.create");
 Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name("product.save");
