d7a93585de9b83b472122e7c83ca09affcb591c6
valchys <vcorrales1@eafit.edu.co>
2025-02-17 04:47:12 -0500

    Activity 10: Add create to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,8 +20,9 @@
         <div class="navbar-nav ms-auto">
           <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
           <a class="nav-link active" href="{{ route('home.about') }}">About</a>
-          <a class="nav-link active" href="{{ route('home.contact') }}">Contact Us</a>
+          <a class="nav-link active" href="{{ route('home.contact') }}">Contact us</a>
           <a class="nav-link active" href="{{ route('product.index') }}">Products</a>
+          <a class="nav-link active" href="{{ route('product.create') }}">Create product</a>
         </div>
       </div>
     </div>
