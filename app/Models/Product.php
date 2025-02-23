e0d705aae38f70db9028a0eca580c25063a99ca9
valchys <vcorrales1@eafit.edu.co>
2025-02-23 03:36:07 -0500

    2. Section B: product model

---
+++ app/Models/Product.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    /**
+     * PRODUCT ATTRIBUTES
+     * $this->attributes['id'] - int - contains the product primary key (id)
+     * $this->attributes['name'] - string - contains the product name
+     * $this->attributes['price'] - int - contains the product price
+     */
+    
+     protected $fillable = ['name','price'];
+     public function getId(): int{return $this->attributes['id'];
+    }
+    
+    public function setId($id) : void
+    {
+        $this->attributes['id'] = $id;
+    }
+    
+    public function getName(): string
+    {
+        return $this->attributes['name'];
+    }
+    
+    public function setName($name) : void
+    {
+        $this->attributes['name'] = $name;
+    }
+    
+    public function getPrice(): int
+    {
+        return $this->attributes['price'];
+    }
+    
+    public function setPrice($price) : void
+    {
+        $this->attributes['price'] = $price;
+    }
+}
