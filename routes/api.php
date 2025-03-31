631c052751b3d70c387619626c25635bcd8590ea
valchys <vcorrales1@eafit.edu.co>
2025-03-31 13:38:34 -0500

    Tutorial 4. Section C: API with Laravel Collection

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,6 @@ Route::get('/products/{id}', 'App\Http\Controllers\Api\ProductApiController@show
 
 Route::get('/v2/products', 'App\Http\Controllers\Api\ProductApiControllerV2@index')->name('api.v2.product.index');
 Route::get('/v2/products/{id}', 'App\Http\Controllers\Api\ProductApiControllerV2@show')->name('api.v2.product.show');
+
+Route::get('/v3/products', 'App\Http\Controllers\Api\ProductApiControllerV3@index')->name('api.v3.product.index');
+Route::get('/v3/products/paginate', 'App\Http\Controllers\Api\ProductApiControllerV3@paginate')->name('api.v3.product.paginate');
