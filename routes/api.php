63965b1ee89e90c4a44bae2f68cb73dee381c5b3
valchys <vcorrales1@eafit.edu.co>
2025-03-31 13:32:30 -0500

    Tutorial 4. Section B: API installation with Laravel Resources

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,6 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/products', 'App\Http\Controllers\Api\ProductApiController@index')->name('api.product.index');
 Route::get('/products/{id}', 'App\Http\Controllers\Api\ProductApiController@show')->name('api.product.show');
+
+Route::get('/v2/products', 'App\Http\Controllers\Api\ProductApiControllerV2@index')->name('api.v2.product.index');
+Route::get('/v2/products/{id}', 'App\Http\Controllers\Api\ProductApiControllerV2@show')->name('api.v2.product.show');
